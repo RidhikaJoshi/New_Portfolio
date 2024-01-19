1e95736f43be171f1c73a04e537d1eea825437f2
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-01-19 22:16:58 +0530

    Add links to social media icons in Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa";
 import { FaMedium } from "react-icons/fa6";
-
+import {Link} from 'react-router-dom'
 
 function Footer() {
   return (
@@ -12,10 +12,10 @@ function Footer() {
 
       <div>
         <ul className='flex flex-row justify-evenly items-center gap-6'>
-          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><FaLinkedin /></li>
-          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><FaGithub /></li>
-          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><FaTwitter /></li>
-          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><FaMedium /></li>
+          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><Link to="https://www.linkedin.com/in/ridhika-joshi-069164221/" target='_blank'><FaLinkedin /></Link></li>
+          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><Link to="https://github.com/RidhikaJoshi" target='_blank'><FaGithub /></Link></li>
+          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><Link to="https://twitter.com/ridhika_joshi" target='_blank'><FaTwitter /></Link></li>
+          <li className='text-2xl border-2 p-1.5 rounded hover:rounded-full border-black'><Link to="https://medium.com/@ridhikasmrh" target='_blank'><FaMedium /></Link></li>
         </ul>
       </div>
       <div className='flex flex-row items-center gap-2'><FaHeart /><div>Made By Ridhika Joshi</div></div>
