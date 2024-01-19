277e42af1853e340ecf3ee6c124db7a9e2c2ff4c
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-01-19 23:14:10 +0530

    Add icons and update text formatting in About component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import photo from '../assets/photo.jpg'
+import { IoBookSharp } from "react-icons/io5";
+import { FaPaintBrush } from "react-icons/fa";
+import { FaPlane } from "react-icons/fa";
 
 function About() {
   return (
-    <div className='min-h-[90vh] w-full bg-green-500 flex items-center justify-center font-serif'>
+    <div className='min-h-[90vh] w-full text-white flex items-center justify-center font-serif'>
 
       <div className='md:w-[90%] w-[95%] min-h-[25rem] flex md:flex-row items-center justify-between flex-col m-2 gap-2'>
           {/* This div is for the text part */}
           <div className='md:w-[55%] flex flex-col justify-between w-[90%] '>
-              <p className='text-2xl font-bold text-center '>LET ME INTRODUCE MYSELF</p>
+              <p className='md:text-2xl font-bold text-center text-xl'>LET ME INTRODUCE MYSELF</p>
               <br/>
               <p>I am Ridhika Joshi from Rishra, West Bengal, India. Currently, I’m pursuing my B.Tech. in Computer Science and Engineering from Academy of Technology, Adisaptagram.</p>
                <br/>
               <p>I embody dynamism, discipline, and a focused work ethic, driven by an unwavering commitment to excellence. With a natural propensity for swift and efficient learning, I actively seek out opportunities to explore diverse domains that pique my curiosity. I am invigorated by engaging challenges and relish problem-solving as a means to simplify complexities, ultimately enhancing the quality of life. My perpetual quest involves continuous skill enhancement and the relentless pursuit of self-improvement.</p>
                <br/>
-              <p>In addition to my passion for Coding and Development, my interests extend into various realms:!</p>
+              <p>In addition to my passion for Coding and Development, my interests extend into various realms:</p>
                <br/>
               <ul>
-                <li> Nourishing my mind through self-help Books</li>
-                <li>Expressing my creativity with a paintbrush in hand</li>
-                <li>Embarking on journeys to new horizons</li>
+                <li className='flex items-center gap-1.5'> <IoBookSharp /><div>Nourishing my mind through self-help Books.</div></li>
+                <li className='flex items-center gap-1.5'><FaPaintBrush /><div>Expressing my creativity with a paintbrush in hand.</div></li>
+                <li className='flex items-center gap-1.5'><FaPlane /><div>Embarking on journeys to new horizons.</div></li>
               </ul>
                <br/>
               <p>“Endeavoring to create impactful contributions that bring about meaningful change.”</p>
