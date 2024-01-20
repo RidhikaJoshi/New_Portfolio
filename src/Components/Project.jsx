4e706542532765c0a090801ef9cd8de080b8f1ae
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-01-20 19:07:21 +0530

    Add new projects and update ProjectCard component

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
+import quickexchange from '../assets/quickexchange.png'
+import passwordgenerator from '../assets/passwordgenerator.png'
+import { FaHtml5,FaCss3Alt ,FaReact} from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiTailwindcss } from "react-icons/si";
 
 function Project() {
   return (
@@ -9,9 +14,14 @@ function Project() {
       <p>Here are a few projects I've worked on recently.</p>
       <br/>
       <div className='md:w-[90%] w-[90%] min-h-[25rem] flex flex-wrap md:flex-row items-center justify-between flex-col m-2 gap-2'>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+
+        <ProjectCard name="Password Generator" photo={passwordgenerator} description="Empower your online security with our Simple Password Generator application. Craft strong and customizable passwords effortlessly, adjusting length and including uppercase, lowercase, numbers, and special characters. With a user-friendly design, generating and copying secure passwords is just a click away. Stay protected with style." github="https://github.com/RidhikaJoshi/Password_Generator"  live="https://ridhikajoshi-password-generator.netlify.app/" icons={[<FaReact key='react' />,<SiTailwindcss key='tailwindcss' />]} />
+
+
+        <ProjectCard name="QuickExchange" photo={quickexchange} description="Created a user-friendly currency converter web app that integrates real-time exchange rates through a third-party API. Users can effortlessly input values and choose both source and target currencies for conversion. The app boasts a responsive design, ensuring a seamless experience on different devices, making currency conversion simple and accessible."
+        github="https://github.com/RidhikaJoshi/QuickExchange"  live="https://ridhikajoshi-quickexchange.netlify.app/" icons={[<FaHtml5 key='html5' />, <FaCss3Alt key='css3' />,<IoLogoJavascript key='javascript' />]}/>
+        {/* <ProjectCard />
+        <ProjectCard /> */}
 
 
       </div>
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { RiLiveFill } from "react-icons/ri";
+import { Link } from 'react-router-dom'
 
-function ProjectCard() {
+function ProjectCard({name,photo,description,icons,live,github}) {
   return (
-    <div className='min-h-96 w-full bg-yellow-700 border-2 rounded-3xl mb-6 p-4'>
+    <div className='md:min-h-96 min-h-[50rem] w-full bg-yellow-700 border-2 rounded-3xl mb-6 p-4'>
 
       {/* This is the heading. */}
-      <p className='md:text-4xl text-3xl font-semibold text-center mb-6'>This is my project.</p>
+      <p className='md:text-4xl text-3xl font-semibold text-center mt-6 mb-6'>{name}</p>
 
 
       <div className='w[full] flex md:flex-row m-2 justify-evenly flex-col'>
 
               {/* This is the project-image section */}
-              <img className='h-96 md:w-[45%] w-full'src="https://images.pexels.com/photos/1666021/pexels-photo-1666021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              <img className='h-96 md:w-[45%] w-full'src={photo} />
 
-
+             
               {/* This is the description of project. */}
-              <div className='md:w-[45%] w-full'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci voluptates repudiandae id recusandae exercitationem voluptate maiores odio nam at numquam porro voluptas dolorum aut possimus iure voluptatem, rerum reprehenderit vitae!</div>
+              <div className='md:w-[45%] w-full text-lg flex flex-col gap-5 md:justify-around '>
+                {description}
+                <div>
+                    <p className='text-2xl font-medium'>Tech Stack: </p>
+                    <div className='flex flex-row justify-start text-4xl gap-3'>
+                                {icons.map((icon, index) => (
+                                <div key={index}>{icon}</div>))}
+                    </div>
+                </div>
+
+                <div className='flex flex-row justify-start text-xl font-medium gap-4'>
+                  <button className='border-2 rounded-xl  flex flex-row items-center gap-2  p-2'><div><Link to={live} target="_blank">Live</Link></div> <RiLiveFill /></button>
+                  <button className='border-2 rounded-xl flex flex-row items-center gap-2 p-2'><div><Link to={github} target="_blank">Github</Link></div> <FaGithub /></button>
+                </div> 
+              </div>
+
+               
       </div>
     </div>
   )
