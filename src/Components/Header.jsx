c4886d002c5bfad423bd2066357dfed1718ca534
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-01-20 22:01:01 +0530

    Update header logo and mobile menu icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import icon from '../assets/icon.ico'
-import { Link,NavLink } from 'react-router-dom'
+import icon from '../assets/my.png'
+import { Link, NavLink } from 'react-router-dom'
 import { useState } from "react";
 import { IoHome } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
@@ -13,71 +13,71 @@ function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <div className='w-full h-14 bg-slate-300 flex justify-around items-center'>
-      <Link to='/'><img className='h-8 w-10' src={icon} /></Link>
+      <Link to='/'><img className='h-10 w-35 rounded-full' src={icon} /></Link>
       <div>
         <ul className='md:flex  hidden justify-evenly items-center font-semibold font-serif text-xl'>
-          <li className='m-4 '><NavLink  to="/" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}>
-                  <div className='flex flex-row items-center gap-1'>
-                  <IoHome />Home</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/about" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><FaUser />About</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/skills" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><RiUserStarFill />Skills</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/projects" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><AiOutlineFundProjectionScreen />Projects</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/education" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><BsFillMortarboardFill />Education</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/contact" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><MdContactMail />Contact</div></NavLink></li>
+          <li className='m-4 '><NavLink to="/" className={({ isActive }) =>
+            ` ${isActive ? "text-slate-500" : "text-red-900"}`}>
+            <div className='flex flex-row items-center gap-1'>
+              <IoHome />Home</div></NavLink></li>
+          <li className='m-4'><NavLink to="/about" className={({ isActive }) =>
+            ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><FaUser />About</div></NavLink></li>
+          <li className='m-4'><NavLink to="/skills" className={({ isActive }) =>
+            ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><RiUserStarFill />Skills</div></NavLink></li>
+          <li className='m-4'><NavLink to="/projects" className={({ isActive }) =>
+            ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><AiOutlineFundProjectionScreen />Projects</div></NavLink></li>
+          <li className='m-4'><NavLink to="/education" className={({ isActive }) =>
+            ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><BsFillMortarboardFill />Education</div></NavLink></li>
+          <li className='m-4'><NavLink to="/contact" className={({ isActive }) =>
+            ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><MdContactMail />Contact</div></NavLink></li>
         </ul>
       </div>
       <section className="MOBILE-MENU flex md:hidden">
-          <div
-            className="HAMBURGER-ICON space-y-2"
-            onClick={() => setIsNavOpen((prev) => !prev)}
-          >
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-          </div>
+        <div
+          className="HAMBURGER-ICON space-y-2"
+          onClick={() => setIsNavOpen((prev) => !prev)}
+        >
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+        </div>
 
-          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-            <div
-              className="absolute top-0 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)}
+        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+          <div
+            className="absolute top-0 right-0 px-8 py-8"
+            onClick={() => setIsNavOpen(false)}
+          >
+            <svg
+              className="h-8 w-8 text-gray-600"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
-              <svg
-                className="h-8 w-8 text-gray-600"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
-            </div>
-            <ul className="flex flex-col items-center justify-between font-semibold font-serif text-xl">
-             <li className='m-4 '><NavLink  to="/" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}>
-                  <div className='flex flex-row items-center gap-1'>
-                  <IoHome />Home</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/about" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><FaUser />About</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/skills" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><RiUserStarFill />Skills</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/projects" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><AiOutlineFundProjectionScreen />Projects</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/education" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><BsFillMortarboardFill />Education</div></NavLink></li>
-          <li className='m-4'><NavLink  to="/contact" className={({isActive})=>
-                ` ${isActive ? "text-slate-500" : "text-red-900" }`}><div className='flex flex-row items-center gap-1'><MdContactMail />Contact</div></NavLink></li>
-            </ul>
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
           </div>
-        </section><style>{`
+          <ul className="flex flex-col items-center justify-between font-semibold font-serif text-xl">
+            <li className='m-4 '><NavLink to="/" className={({ isActive }) =>
+              ` ${isActive ? "text-slate-500" : "text-red-900"}`}>
+              <div className='flex flex-row items-center gap-1'>
+                <IoHome />Home</div></NavLink></li>
+            <li className='m-4'><NavLink to="/about" className={({ isActive }) =>
+              ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><FaUser />About</div></NavLink></li>
+            <li className='m-4'><NavLink to="/skills" className={({ isActive }) =>
+              ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><RiUserStarFill />Skills</div></NavLink></li>
+            <li className='m-4'><NavLink to="/projects" className={({ isActive }) =>
+              ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><AiOutlineFundProjectionScreen />Projects</div></NavLink></li>
+            <li className='m-4'><NavLink to="/education" className={({ isActive }) =>
+              ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><BsFillMortarboardFill />Education</div></NavLink></li>
+            <li className='m-4'><NavLink to="/contact" className={({ isActive }) =>
+              ` ${isActive ? "text-slate-500" : "text-red-900"}`}><div className='flex flex-row items-center gap-1'><MdContactMail />Contact</div></NavLink></li>
+          </ul>
+        </div>
+      </section><style>{`
       .hideMenuNav {
         display: none;
       }
