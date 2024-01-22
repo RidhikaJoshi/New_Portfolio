75b07051f455fc63786122b42315f319cfc50719
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-01-23 04:21:35 +0530

    Add new skills and coding profiles

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -10,36 +10,76 @@ import html from '../assets/html.png'
 import css from '../assets/css.png'
 import js from '../assets/javascript.png'
 import redux from '../assets/redux.png'
+import git from '../assets/Git.png'
+import github from '../assets/Github.png'
+import vscode from '../assets/vs-code.png'
+import netlify from '../assets/netlify.png'
+import heroku from '../assets/heroku.png'
+import tailwind from '../assets/tailwind-css.png'
+import bootstrap from '../assets/Bootstrap.png'
+import chakra from '../assets/chakra.png'
+import firebase from '../assets/firebase.png'
+import leetcode from '../assets/leetcode.png'
+import codeforces from '../assets/codeforces.png'
+import GFG from '../assets/GFG.png'
+import codechef from '../assets/codechef.png'
+import hackerrank from '../assets/hackerrank.png'
+import dsa from '../assets/DSa.png'
+import postman from '../assets/postman.png'
+import { FaLaptopCode } from "react-icons/fa6";
+import {Link } from 'react-router-dom'
 
 function Skills() {
   return (
-   <div className='min-h-[80vh] w-full text-white  flex flex-col items-center justify-center font-serif'>
+    <div className='min-h-[80vh] w-full text-white  flex flex-col items-center justify-center font-serif'>
 
-        {/* This divison is for the heading and gif */}
-          <div className=' min-h-[90vh] w-[90%] bg-green-400 flex  flex-col justify-center items-center md:mt-4 mb-4 gap-6'>
+     
+      <div className=' min-h-[90vh] md:w-[80%] w-[90%] flex  flex-col justify-center items-center mt-8 mb-4 '>
 
-              <h1 className='md:text-4xl text-2xl font-bold text-[#03F6FF]'>Skills & Abilities</h1>
-
-
-              {/* This division contains tech-stack */}
-            
-              <div className=' w-full min-h-96  flex-wrap flex items-center justify-evenly p-4 gap-4 bg-red-800 '> 
-             
-                     
-                       <SkillsCard icon={cplus} name="C++"/>
-                       <SkillsCard icon={c} name="C"/>
-                        <SkillsCard icon={java} name="Java"/>
-                         <SkillsCard icon={python} name="Python"/>
-                         <SkillsCard icon={html} name="HTML"/>
-                          <SkillsCard icon={css} name="CSS"/>
-                          <SkillsCard icon={js} name="JavaScript"/>
-                          <SkillsCard icon={reacticon} name="React Js"/>
-                          <SkillsCard icon={redux} name="Redux"/>
+        <h1 className='md:text-4xl text-2xl flex flex-row items-center gap-2 font-bold text-[#03F6FF]'><FaLaptopCode /><div>Skills & Abilities</div></h1>
 
 
+        {/* This division contains tech-stack */}
 
-              </div>
+          <div className=' w-full min-h-96  flex-wrap flex items-center justify-between p-10 gap-6 '>
+
+
+              <SkillsCard icon={cplus} name="C++" />
+              <SkillsCard icon={c} name="C" />
+              <SkillsCard icon={java} name="Java" />
+              <SkillsCard icon={python} name="Python" />
+              <SkillsCard icon={html} name="HTML" />
+              <SkillsCard icon={css} name="CSS" />
+              <SkillsCard icon={js} name="JavaScript" />
+              <SkillsCard icon={reacticon} name="React Js" />
+               <SkillsCard icon={firebase} name="Firebase" />
+              <SkillsCard icon={tailwind} name="Tailwind" />
+              <SkillsCard icon={bootstrap} name="Bootstrap" />
+              <SkillsCard icon={chakra} name="Chakra-UI" />
+              <SkillsCard icon={redux} name="Redux" />
+              <SkillsCard icon={git} name="Git" />
+              <SkillsCard icon={github} name="Github" />
+              <SkillsCard icon={postman} name="Postman" />
+              <SkillsCard icon={dsa} name="DSA" />
+              <SkillsCard icon={vscode} name="VS Code" />
+              <SkillsCard icon={netlify} name="Netlify" />
+              <SkillsCard icon={heroku} name="Heroku" />
+
           </div>
+          <br/>
+          <p className='md:text-4xl text-2xl flex flex-row items-center gap-2 font-bold text-[#03F6FF]'><FaLaptopCode /><div>Coding Profiles</div></p>
+
+           <div className=' w-full mt-4  flex-wrap flex items-center justify-between p-10 gap-6 '>
+
+            <Link to="https://leetcode.com/RIDHIKA_JOSHI/" target="_blank"><SkillsCard icon={leetcode} name="LeetCode" /></Link>
+            <Link to="https://codeforces.com/profile/ridhika17" target="_blank"><SkillsCard icon={codeforces} name="Codeforces" /></Link>
+            <Link to="https://auth.geeksforgeeks.org/user/qmq0dxjk1xbl7bp49wz1cmicj5xcpcqfmn6vd7oe" target="_blank"><SkillsCard icon={GFG} name="GFG" /></Link>
+            <Link to="https://www.codechef.com/users/ridhikajoshi17" target="_blank"><SkillsCard icon={codechef} name="Codechef" /></Link>
+             <Link to="https://www.hackerrank.com/profile/ridhikasmrh" target="_blank"><SkillsCard icon={hackerrank} name="Hackerrank" /></Link>
+            
+           </div>
+
+      </div>
 
     </div>
   )
--- src/Components/miscellaneous/SkillsCard.jsx
+++ src/Components/miscellaneous/SkillsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SkillsCard({icon,name}) {
   return (
     
-    <div className='h-32 w-32 bg-black flex flex-col justify-evenly items-center text-2xl  rounded-xl'>
+    <div className='h-[8.5rem] w-[8.5rem] bg-black flex flex-col border-[1px] hover:shadow-lg hover:shadow-cyan-500/50 border-[#03F6FF] justify-evenly items-center text-2xl  rounded-xl'>
         <img className='h-20 w-20' src={icon}/>
         <span className='text-2xl'>{name}</span>
     </div>
