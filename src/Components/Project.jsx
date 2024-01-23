f987b599571bdb0dd5c1719044ccf82ad30bd573
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-01-23 17:39:32 +0530

    Refactor image placement in Project component

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -218,7 +218,7 @@ function Project() {
                 </g>
               </svg> */}
 
-              <Slide direction='right'><Tilt><img className='' src="https://user-images.githubusercontent.com/74038190/219923809-b86dc415-a0c2-4a38-bc88-ad6cf06395a8.gif"></img></Tilt></Slide>
+              <Tilt><img className='' src="https://user-images.githubusercontent.com/74038190/219923809-b86dc415-a0c2-4a38-bc88-ad6cf06395a8.gif"></img></Tilt>
             </div>
         <div className='  md:w-[50%] w-full flex flex-col items-center md:justify-center gap-4'>
           <Slide direction='left'><h1 className='md:text-4xl font-bold mt-8 text-2xl text-center text-[#03F6FF]' >My Recent Projects</h1></Slide>
