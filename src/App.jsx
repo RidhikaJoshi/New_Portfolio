e0060134b695e7e2fbbb7abacdc33016ca1930e6
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-01-24 20:20:45 +0530

    Add Preloader component for loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import './App.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import PreLoader from './Components/Preloader'
 
 
 function App() {
-
+const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  if (loading) return <PreLoader />;
+  
   return (
     <div >
       <Header/>
