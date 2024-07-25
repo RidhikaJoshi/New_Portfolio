c53a70a795563fdea02b6e53e793c98efcd5ed2c
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-07-25 16:18:05 +0530

    Add TouristNest and MusicAcademy projects

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -3,7 +3,10 @@ import ProjectCard from './ProjectCard';
 import quickexchange from '../assets/quickexchange.png';
 import passwordgenerator from '../assets/passwordgenerator.png';
 import anagram from '../assets/anagram.png';
+import touristNest from '../assets/TouristNest.png';
+import musicAcademy from '../assets/musicAcademy.png';
 import { FaHtml5,FaCss3Alt ,FaReact} from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
 import { IoLogoJavascript } from "react-icons/io5";
 import { SiTailwindcss } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
@@ -36,17 +39,20 @@ function Project() {
 
       <ProjectCard name="VideoTweets" photo={videotweets} description="VideoTweets offers seamless sharing and discovery of multimedia content, including videos and tweets. Users can comment, like, and engage with content, create playlists, manage profiles, and subscribe to channels. Built on Node.js with Express.js, MongoDB, and Mongoose, it ensures secure authentication and authorization for users." github="https://github.com/RidhikaJoshi/VideoTweets"  live="https://documenter.getpostman.com/view/18992770/2sA2xpR8nu" icons={[<FaNodeJs key='NodeJs'/>,<SiExpress key='Express' />,<SiMongodb  key='Mongodb' />]} />
 
+      <ProjectCard name="TouristNest" photo={touristNest} description="TouristNest offers secure user authentication for signing up, logging in, and logging out. Users can browse hotels, view details, make bookings, and manage their reservations seamlessly. The platform also supports Stripe for secure payment processing and is designed to provide an optimal experience across all devices." github="https://github.com/RidhikaJoshi/TouristNest"  live="https://touristnest.onrender.com/" icons={[<FaNodeJs key='NodeJs'/>,<SiExpress key='Express' />,<SiMongodb  key='Mongodb' />,<FaReact key='react' />,<SiTailwindcss key='tailwindcss' />]} />
+
       <ProjectCard name="BlogSphere" photo={blog} description="BlogSphere is a web application crafted for users to articulate their thoughts and ideas through published articles. It seamlessly incorporates user authentication, a robust rich text editor, and Appwrite as the backend, ensuring a smooth and secure experience for writers and readers." github="https://github.com/RidhikaJoshi/Blog"  live="https://blogsphere-swf7.onrender.com/" icons={[<SiAppwrite key='Appwrite'/>,<FaReact key='react' />,<SiTailwindcss key='tailwindcss' />]} />
 
-        <ProjectCard name="Password Generator" photo={passwordgenerator} description="Empower your online security with our Simple Password Generator application. Craft strong and customizable passwords effortlessly, adjusting length and including uppercase, lowercase, numbers, and special characters. With a user-friendly design, generating and copying secure passwords is just a click away. Stay protected with style." github="https://github.com/RidhikaJoshi/Password_Generator"  live="https://ridhikajoshi-password-generator.netlify.app/" icons={[<FaReact key='react' />,<SiTailwindcss key='tailwindcss' />]} />
+      <ProjectCard name="MusicAcademy" photo={musicAcademy} description="The Music Academy website, built with Next.js, offers a comprehensive list of music courses, allowing users to explore various genres and instruments. The site features a responsive design to ensure an optimal user experience across all devices." github="https://github.com/RidhikaJoshi/music"  live="https://music-xi.vercel.app/" icons={[<RiNextjsFill key='NextjsFill'/>,<SiTailwindcss key='tailwindcss' />]} />
+
+      <ProjectCard name="Password Generator" photo={passwordgenerator} description="Empower your online security with our Simple Password Generator application. Craft strong and customizable passwords effortlessly, adjusting length and including uppercase, lowercase, numbers, and special characters. With a user-friendly design, generating and copying secure passwords is just a click away. Stay protected with style." github="https://github.com/RidhikaJoshi/Password_Generator"  live="https://ridhikajoshi-password-generator.netlify.app/" icons={[<FaReact key='react' />,<SiTailwindcss key='tailwindcss' />]} />
 
 
-        <ProjectCard name="QuickExchange" photo={quickexchange} description="Created a user-friendly currency converter web app that integrates real-time exchange rates through a third-party API. Users can effortlessly input values and choose both source and target currencies for conversion. The app boasts a responsive design, ensuring a seamless experience on different devices, making currency conversion simple and accessible."
-        github="https://github.com/RidhikaJoshi/QuickExchange"  live="https://ridhikajoshi-quickexchange.netlify.app/" icons={[<FaHtml5 key='html5' />, <FaCss3Alt key='css3' />,<IoLogoJavascript key='javascript' />]}/>
+      <ProjectCard name="QuickExchange" photo={quickexchange} description="Created a user-friendly currency converter web app that integrates real-time exchange rates through a third-party API. Users can effortlessly input values and choose both source and target currencies for conversion. The app boasts a responsive design, ensuring a seamless experience on different devices, making currency conversion simple and accessible."
+      github="https://github.com/RidhikaJoshi/QuickExchange"  live="https://ridhikajoshi-quickexchange.netlify.app/" icons={[<FaHtml5 key='html5' />, <FaCss3Alt key='css3' />,<IoLogoJavascript key='javascript' />]}/>
 
 
-
-        <ProjectCard name="Anagram Quest Game" photo={anagram} description="Dive into the world of language and fun with Anagram Quest—an engaging word puzzle game that challenges your brain! Unscramble jumbled letters to discover meaningful words, track your scores, and race against time with the built-in timer for an added thrill. Enjoy the game's interactive sound effects, and experience the ultimate word-solving adventure with responsive design across various screen sizes." github="https://github.com/RidhikaJoshi/Anagram_Quest"  live="https://anagram-quest.netlify.app/" icons={[<FaHtml5 key='html5' />, <FaCss3Alt key='css3' />,<IoLogoJavascript key='javascript' />]} />
+      <ProjectCard name="Anagram Quest Game" photo={anagram} description="Dive into the world of language and fun with Anagram Quest—an engaging word puzzle game that challenges your brain! Unscramble jumbled letters to discover meaningful words, track your scores, and race against time with the built-in timer for an added thrill. Enjoy the game's interactive sound effects, and experience the ultimate word-solving adventure with responsive design across various screen sizes." github="https://github.com/RidhikaJoshi/Anagram_Quest"  live="https://anagram-quest.netlify.app/" icons={[<FaHtml5 key='html5' />, <FaCss3Alt key='css3' />,<IoLogoJavascript key='javascript' />]} />
 
 
       </div>
